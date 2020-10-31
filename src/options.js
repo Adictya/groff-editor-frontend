36f266c9f4d2c6863751333c338716995ef8f0e5
adictya <aditya.26101@gmail.com>
2020-10-31 20:12:10 +0530

    testing routes

--- src/options.js
+++ src/options.js
@@ -1,6 +1,8 @@
 const options = {
-	apiUrl: "https://groff.dscvit.com/api/",
-	wssUrl: "https://groff.dscvit.com",
+	// apiUrl: "https://groff.dscvit.com/api/",
+	// wssUrl: "https://groff.dscvit.com",
+	apiUrl: "http://localhost:3000/api/",
+	wssUrl: "localhost:3000/",
 };
 
 export default options;
