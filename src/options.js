cdd6bc9303105f477236fb2227172c491e560fae
adictya <aditya.26101@gmail.com>
2020-11-04 00:37:48 +0530

    Testing

--- src/options.js
+++ src/options.js
@@ -1,6 +1,7 @@
 const options = {
-	apiUrl: "https://groff.dscvit.com/api/",
-	wssUrl: "https://groff.dscvit.com",
+	// apiUrl: "https://groff.dscvit.com/api/",
+	apiUrl: "http://13.234.111.67/api/",
+	wssUrl: "13.234.111.67/",
 };
 
 export default options;
